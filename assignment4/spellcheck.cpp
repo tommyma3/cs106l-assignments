8e3b4aedcf43a2d7be7b1aac9cecf8a4292c1834
CutiRomero02 <124090467@link.cuhk.edu.cn>
2025-12-13 14:03:28 +0800

    done with a4

--- assignment4/spellcheck.cpp
+++ assignment4/spellcheck.cpp
@@ -12,58 +12,39 @@ std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
   /* TODO: Implement this method */
-
-  std::vector<std::string::iterator> positions = find_all(source.begin(), source.end(), 
-    [](char c) { return std::isspace(static_cast<unsigned char>(c)); });
   Corpus tokens;
-  
-/*
-  for (size_t i = 0; i < positions.size() - 1; ++i) {
-    Token token(source, positions[i], positions[i + 1]);
-    if (!token.content.empty()) {
-      tokens.insert(token);
-    }
-  }
-*/
-  
-  if (positions.size() >= 2) {
-    std::transform(positions.begin(), positions.end() - 1, positions.begin() + 1, 
-    std::inserter(tokens, tokens.end()),
-    [&source](auto it1, auto it2) {
-      return Token{source, it1, it2};
-    });
-  
-   std::erase_if(tokens, [](const auto& token){ 
-      return token.content.empty(); 
-    });  
-  }
+
+  auto spaces = find_all(source.begin(), source.end(), [] (char x) {return std::isspace(x);});
+
+  std::insert_iterator<Corpus> tokens_insert = std::insert_iterator(tokens, tokens.end());
+
+  auto out_range = std::transform(spaces.begin(), spaces.end() - 1, spaces.begin() + 1, tokens_insert, 
+      [&source] (auto x, auto y) {return Token(source, x, y);});
+
+  std::erase_if(tokens, [](const auto& token) {return token.content.empty();});
 
   return tokens;
 
 }
 
+
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
   /* TODO: Implement this method */
-  
+
   namespace rv = std::ranges::views;
+
   auto view = source 
-    | rv::filter([&dictionary](const auto& token) {
-        return !dictionary.contains(token.content);
-        })
-    | rv::transform([&dictionary](const auto& token) {
-        auto suggestionView = dictionary | rv::filter([&token](const auto& word) {
-              return levenshtein(token.content, word) == 1;
-            });
-        std::set<std::string> suggestions(suggestionView.begin(), suggestionView.end());
-        return Misspelling{token, suggestions};
-      })
-    | rv::filter([](const auto& misspelling) {
-        return !misspelling.suggestions.empty();
-      });
+      | rv::filter([&dictionary] (const Token& token) {return !dictionary.contains(token.content);})
+      | rv::transform([&dictionary] (const Token& token) {
+            auto mispell_view = dictionary 
+                                  | rv::filter([&token] (const std::string& vocab) {return levenshtein(token.content, vocab) == 1;});
+            std::set<std::string> suggestions(mispell_view.begin(), mispell_view.end());
+            return Misspelling(token, suggestions);
+          })
+      | rv::filter([] (const Misspelling& mispelling) {return !mispelling.suggestions.empty();});
   
-  std::set<Misspelling> misspellings(view.begin(), view.end());
-  return misspellings;
-    
+  std::set<Misspelling> misses(view.begin(), view.end());
+  return misses;
 };
 
 /* Helper methods */
